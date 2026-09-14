2c0a841af6240497a31cce509b8e3601676f6240
Javier López <lopezchaconjavier@fpllefia.com>
2026-09-14 20:33:10 +0200

    inicio

---
+++ script.js
@@ -0,0 +1,17 @@
+let nomJugador
+
+console.log("Hola!")
+
+const MAX_TIRADES = 5
+
+nomJugador = "Javier"
+
+// MAX_TIRADES = 3
+
+alert("Jugador: " + nomJugador)
+
+alert(MAX_TIRADES)
+
+function sumar(a, b) {
+    return a + b
+}
