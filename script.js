d5067c77cff4c7647bcf60e95559d158cf4d8fd6
Javier López <lopezchaconjavier@fpllefia.com>
2026-09-15 17:55:27 +0200

    j3i

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-let nomJugador
+let nomJugador;
 
-let tablero = document.querySelector("#tablero")
+let tablero = document.querySelector("#tablero");
 
-let numero = document.querySelector("#numero")
+let numero = document.querySelector("#numero");
 
-let tableroAlternativo = document.querySelector("#tableroAlternativo")
+let tableroAlternativo = document.querySelector("#tableroAlternativo");
 
-const boton = document.querySelector("#boton")
+let tablaMultiplicar = document.querySelector("#tablaMultiplicar");
 
-console.log("Hola!")
+const boton = document.querySelector("#boton");
 
-const MAX_TIRADES = 5
+console.log("Hola!");
 
-nomJugador = "Javier"
+const MAX_TIRADES = 5;
+
+nomJugador = "Javier";
 
 // MAX_TIRADES = 3
 
-alert("Jugador: " + nomJugador)
+alert("Jugador: " + nomJugador);
 
-alert(MAX_TIRADES)
+alert(MAX_TIRADES);
 
 function sumar(a, b) {
-    return a + b
+  return a + b;
 }
 
 function multiplicar(a, b) {
-    return a * b
+  return a * b;
 }
 
 boton.addEventListener("click", () => {
-    tablero.innerHTML = sumar(2, 3);
-    tableroAlternativo.innerHTML = multiplicar(2, 3)
+  tablero.innerHTML = sumar(2, 3);
+  tableroAlternativo.innerHTML = multiplicar(2, 3);
 });
 
 numero.addEventListener("change", () => {
-    
-})
+  tablaMultiplicar.innerHTML = "";
+  if (numero.value <= 0) {
+    tablaMultiplicar.innerHTML = "El número debe ser mayor a 0";
+  } else if (numero.value >= 11) {
+    tablaMultiplicar.innerHTML = "El número debe ser menor o igual a 10";
+  } else {
+    for (let i = 0; i <= 10; i++) {
+      tablaMultiplicar.innerHTML += "<p>" + numero.value + " x " + i + " = " + numero.value * i + "</p>";
+    }
+  }
+});
