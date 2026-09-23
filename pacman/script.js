776a19740bbcbd0733f4e4aff6b4f46844b73508
Javier López <lopezchaconjavier@fpllefia.com>
2026-09-23 19:05:16 +0200

    commit

--- pacman/script.js
+++ pacman/script.js
@@ -1,10 +1,12 @@
-const PantallaDesaparecera =  document.querySelector("#PantallaQueDesaparecera")
+const PantallaDesaparecera = document.querySelector("#PantallaQueDesaparecera");
 
-const botonInciarEljuego = documen.querySelector("#botonInciarEljuego")
+const botonInciarEljuego = document.querySelector("#botonInciarEljuego");
 
-const PantallaQueVaApararecer = document.querySelector("#PantallaQueVaApararecer")
+const PantallaQueVaApararecer = document.querySelector(
+  "#PantallaQueVaApararecer",
+);
 
 botonInciarEljuego.addEventListener("click", () => {
-    PantallaDesaparecera.classList.remove("hidden")
-    PantallaQueVaApararecer.classList.add("hidden")
-})
+  PantallaDesaparecera.classList.remove("hidden");
+  PantallaQueVaApararecer.classList.add("hidden");
+});
